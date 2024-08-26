42c10374a16a087c21ca4f2d39bd10c825f4552b
WILLINTONG SANCHEZ <SANCHEZNW@previsora.col>
2024-08-26 18:00:27 -0500

    clase numero 32 Api Rest enviar Json peticion Post

--- src/main/java/com/wsan/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/wsan/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,19 +3,35 @@ package com.wsan.springboot.webapp.springboot_web.controllers;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.wsan.springboot.webapp.springboot_web.models.User;
 import com.wsan.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
 
+    @Value("${config.username}")
+    private String username;
+
+    //@Value("${config.message}")
+    //private String message;
+
+    @Value("${config.listOfValues}")
+    private String[] listOfValues;
+
+    @Value("${config.code}")
+    private Integer code;
+
     @GetMapping("/baz/{message}")
-    public ParamDto baz(@PathVariable String message){
+    public ParamDto baz(@PathVariable String message) {
 
         ParamDto param = new ParamDto();
         param.setMensaje(message);
@@ -23,10 +39,28 @@ public class PathVariableController {
     }
 
     @GetMapping("/mix/{product}/{id}")
-    public Map<String, Object> mixPathVar (@PathVariable String product, @PathVariable Long id){
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
         Map<String, Object> json = new HashMap<>();
         json.put("product", product);
         json.put("id", id);
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user) {
+        // implementar
+        user.setName(user.getName().toUpperCase());
+        return user;
+    }
+
+    @GetMapping("/values")
+    public Map<String, Object> values(@Value("${config.message}") String message) {
+
+        Map<String, Object> json = new HashMap<>();
+        json.put("username", username);
+        json.put("message", message);
+        json.put("code", code);
+        json.put("listOfValues", listOfValues);
         return json;
     }
 }
